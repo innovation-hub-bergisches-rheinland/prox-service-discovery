083710bf6dec6992016cb16619c4f2f18c031898
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-06-12 16:18:31 +0200

    Rename Java package to prox

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM openjdk:8u191-jdk-alpine3.8
+
 ARG DEPENDENCY=target/dependency
 COPY ${DEPENDENCY}/BOOT-INF/lib /app/lib
 COPY ${DEPENDENCY}/META-INF /app/META-INF
 COPY ${DEPENDENCY}/BOOT-INF/classes /app
-ENTRYPOINT ["java","-cp","app:app/lib/*","io.archilab.projektboerse.servicediscovery.ServiceDiscoveryApplication"]
+
+ENTRYPOINT ["java","-cp","app:app/lib/*","io.archilab.projektboerse.servicediscovery.ServiceDiscovery"]
--- src/main/java/io/archilab/prox/servicediscovery/ServiceDiscovery.java
+++ src/main/java/io/archilab/prox/servicediscovery/ServiceDiscovery.java
@@ -1,4 +1,4 @@
-package io.archilab.projektboerse.servicediscovery;
+package io.archilab.prox.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ServiceDiscoveryApplication {
+public class ServiceDiscovery {
 
   public static void main(String[] args) {
-    SpringApplication.run(ServiceDiscoveryApplication.class, args);
+    SpringApplication.run(ServiceDiscovery.class, args);
   }
 
 }
