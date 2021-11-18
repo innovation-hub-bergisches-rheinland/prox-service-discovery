3ce361490e464141ffeff60edac072db188b9671
Tobias Hund <tobias.hund@innovation-hub.de>
2021-11-18 18:04:46 +0100

    Apply Formatting

--- src/main/java/de/innovationhub/prox/servicediscovery/ServiceDiscovery.java
+++ src/main/java/de/innovationhub/prox/servicediscovery/ServiceDiscovery.java
@@ -1,5 +1,6 @@
 package de.innovationhub.prox.servicediscovery;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
