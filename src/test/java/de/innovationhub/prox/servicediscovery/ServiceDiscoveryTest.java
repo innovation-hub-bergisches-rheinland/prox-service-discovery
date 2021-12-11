6dba8426189a56b417a64b34857c402594befbd3
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-12-11 19:32:02 +0100

    style: apply formatting

--- src/test/java/de/innovationhub/prox/servicediscovery/ServiceDiscoveryTest.java
+++ src/test/java/de/innovationhub/prox/servicediscovery/ServiceDiscoveryTest.java
@@ -1,5 +1,6 @@
 package de.innovationhub.prox.servicediscovery;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ServiceDiscoveryTest {
 
   @Test
-  void contextLoads() {
-
-  }
+  void contextLoads() {}
 }
